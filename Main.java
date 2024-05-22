ce971b32ff56195ae19a699d07d456833f3054fd
Rohil Gugunta <108376638+RGcoderA@users.noreply.github.com>
2024-05-21 21:23:31 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Rohil Gugunta";
+
+        // Print the array
+        System.out.println(Arrays.toString(arr));
+
+        // Print the number
+        System.out.println(num);
+
+        // Print the name
+        System.out.println(name);
+
+        String a = "Rohil";
+        System.out.println(a);
+        a = "Gugunta";
+        System.out.println(a);
+    }
+}
